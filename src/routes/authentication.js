f75e05dd96542bb71636e0e0048d5ac6a709dec1
Gustavo Herrera <tavoxr23@gmail.com>
2020-02-17 23:08:17 -0600

    get sign up

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,6 +2,14 @@ const express = require('express')
 
 const router= express.Router()
 
+router.get('/signup',(req,res)=>{
+
+    res.render('auth/signup')
+})
+
+router.post('/signup',async(req,res)=>{
+
+})
 
 
 
